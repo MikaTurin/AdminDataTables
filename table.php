bd1c75f1f1d2fcd215a786665fe38948ccbabe8e
MikaTurin <mika.turin@gmail.com>
2014-08-12 17:15:21 +0300

    php updated

--- table.php
+++ table.php
@@ -1,5 +1,24 @@
 <?php
-$r = $this->readData(); //you data returned as array
+$r = array(0 => array(
+    '1083',
+    'view',
+    '100',
+    '50',
+    '77',
+    'cf024a2783440f93992cf53612cf39a76f3eff07',
+    '2014-08-08 21:35:39',
+    '2014-08-11 14:03:12',
+    '0-0-0',
+    '0',
+    'SU: O4HG',
+    '7777',
+    'MDVQEZ',
+    'MOW-LED',
+    '2014-08-25',
+    '2014-08-31',
+    'ADT:1',
+    '192.168.212.176'
+));
 $a = new stdClass();
 $a->aaData = $r;
 
